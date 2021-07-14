9095a2468a96796d37df36f843664b5e7817fb08
ivan kopech <ivankopech@gmail.com>
2021-07-14 12:11:20 -0300

    sexto elemento especificado

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var buscar = document.querySelector('.lupa');
 var grados = document.querySelector('.grados');
 var tempMaxima = document.querySelector('.temp-maxima');
 var tempMinima = document.querySelector('.temp-minima');
-texto = document.querySelector('.busqueda');
+var texto = document.querySelector('.busqueda');
 var clima = document.querySelector('.descripcion');
 var fecha = document.querySelector('.dia');
 var viento = document.querySelector('.viento');
 var amanecer = document.querySelector('.amanecer');
 var atardecer = document.querySelector('.anochecer');
+var presion = document.querySelector('.presion');
 var humedad = document.querySelector('.humedad');
 var visibilidad = document.querySelector('.visibilidad');
 
@@ -36,10 +37,11 @@ function climaHoy(){
         var visibilidadValor = data.list[0].visibility;
         var tempMaximaValor = data.list[0].main.temp_max;
         var tempMinimaValor = data.list[0].main.temp_min;
+        var presionValor = data.list[0].main.pressure;
         
         var fechaValor = data.list[0].dt;
         var date = new Date(fechaValor * 1000);
-        var dia = date.getDate();
+        var dia = date.getDate().toString();
         var mes = date.getMonth()+1;
         var año = date.getFullYear();
         var lat = data.city.coord.lat;
@@ -51,6 +53,7 @@ function climaHoy(){
         amanecer.innerHTML = amanecerHora +':'+ amanecerMinutos +' AM';
         atardecer.innerHTML = atardecerHora +':'+ atardecerMinutos +' PM';
         humedad.innerHTML = humedadValor + '%';
+        presion.innerHTML = presionValor + 'Pa';
         tempMaxima.innerHTML = tempMaximaValor + 'C°';
         tempMinima.innerHTML = tempMinimaValor + 'C°';
         visibilidad.innerHTML = visibilidadValor + ' m';
